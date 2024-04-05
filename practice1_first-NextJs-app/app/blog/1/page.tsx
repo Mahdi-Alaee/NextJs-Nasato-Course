a40f507697385aa74824a648face9930375e30f1
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-04-05 19:30:52 +0330

    practice 1 : add blog pages

---
+++ practice1_first-NextJs-app/app/blog/1/page.tsx
@@ -0,0 +1,8 @@
+export default function page(){
+    return (
+        <>
+            <h1>Blog 1</h1>
+            <p>NextJs is a react-based framework for better web developing</p>
+        </>
+    )
+}
--- practice1_first-NextJs-app/app/blog/page.tsx
+++ practice1_first-NextJs-app/app/blog/page.tsx
@@ -1,11 +1,11 @@
 import Link from "next/link";
 
-export default function HomePage () {
+export default function BlogPage(){
     return (
         <>
-            <h1>this is my first next page</h1>
+            <h1>this is Blog page</h1>
             <p>NextJs is a react-based framework for better web developing</p>
-            <Link href='/blog' >go to blog</Link>
+            <Link href='/blog/1' >go to blog 1</Link>
         </>
     )
 }
