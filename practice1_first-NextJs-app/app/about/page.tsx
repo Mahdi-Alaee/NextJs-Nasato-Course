01d7849c2b8a6766a33848045ef2c1a10edacb34
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-04-05 19:49:08 +0330

    practice 1 : add about page

---
+++ practice1_first-NextJs-app/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <>
+      <h1>this is about page</h1>
+      <p>NextJs is a react-based framework for better web developing</p>
+    </>
+  );
+}
