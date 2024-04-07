269fde3727b584071ce7776d9e5fdc022a4086b6
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-04-07 18:16:58 +0330

    practice 1 : styling with css module

--- practice1_first-NextJs-app/app/page.tsx
+++ practice1_first-NextJs-app/app/page.tsx
@@ -1,9 +1,9 @@
-import Link from "next/link";
+import styles from './app.module.css';
 
 export default function HomePage () {
     return (
         <>
-            <h1>this is my first next page</h1>
+            <h1 className={styles.center}>this is my first next page</h1>
             <p>NextJs is a react-based framework for better web developing</p>
         </>
     )
