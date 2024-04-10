5b052898bcfc67a68883512f72a793c93c0a7bca
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-04-10 08:44:38 +0330

    practice 1 : read and show hollow knight article from external markdown file

--- practice1_first-NextJs-app/app/reviews/hollow-knight/page.tsx
+++ practice1_first-NextJs-app/app/reviews/hollow-knight/page.tsx
@@ -1,12 +1,22 @@
+import { readFile } from "node:fs/promises";
 import Heading from "@/components/Heading";
+import { marked } from "marked";
 
-export default function HollowKnightReviewPage () {
+export default async function HollowKnightReviewPage() {
+  const text = await readFile("./content/reviews/hollow-knight.md", 'utf-8');
+  const html = marked(text)
+
   return (
     <>
       <Heading>this is Hollow-Knight review page</Heading>
-      <img className="my-4 rounded" src="/images/hollow-knight.jpg" 
-      width='600' height='300' alt="game image" />
-      <p>NextJs is a react-based framework for better web developing</p>
+      <img
+        className="my-4 rounded"
+        src="/images/hollow-knight.jpg"
+        width="600"
+        height="300"
+        alt="game image"
+      />
+      <article dangerouslySetInnerHTML={{__html: html}} />
     </>
   );
 }
---
+++ practice1_first-NextJs-app/content/reviews/hollow-knight.md
@@ -0,0 +1,7 @@
+# welcome to the __hollow knight__ _review_
+
+__game advantages__: 
+
+* performance
+* ping
+* agility
