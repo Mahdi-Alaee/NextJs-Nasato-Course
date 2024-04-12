fb42bf961f63004230ed99a254ca29c7ed3503a1
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-04-12 18:41:31 +0330

    practice 1 : review dynamic route

---
+++ practice1_first-NextJs-app/app/reviews/[slug]/page.tsx
@@ -0,0 +1,43 @@
+import Heading from "@/components/Heading";
+import { getReview } from "@/lib/reviews";
+
+interface ReviewPageProps {
+  params: { slug: string };
+}
+
+export default async function ReviewPage({
+  params: { slug },
+}: ReviewPageProps) {
+  let reviewData;
+
+  try {
+    reviewData = await getReview(slug);
+  } catch (err) {
+    console.log(err);
+  }
+
+  return (
+    <>
+      {reviewData ? (
+        <>
+          <Heading>{reviewData.title}</Heading>
+          {/* date */}
+          <p className="italic mt-2 -mb-2">{reviewData.date}</p>
+          <img
+            className="my-4 rounded"
+            src={reviewData.image}
+            width="600"
+            height="300"
+            alt="game image"
+          />
+          <article
+            dangerouslySetInnerHTML={{ __html: reviewData.body }}
+            className="prose prose-slate prose-h1:text-3xl"
+          />
+        </>
+      ) : (
+        <h1 className="bg-red-200 text-red-600 flex justify-center items-center text-5xl h-full">not found</h1>
+      )}
+    </>
+  );
+}
