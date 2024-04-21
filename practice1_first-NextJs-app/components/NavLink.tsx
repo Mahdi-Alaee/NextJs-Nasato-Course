0787e6886916536a4967b5739621c09c392524bc
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-04-21 16:22:02 +0330

    practice 1 : coding NavLink component

---
+++ practice1_first-NextJs-app/components/NavLink.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { ReactNode } from "react";
+
+interface NavLinkProps {
+  href: string;
+  children: ReactNode;
+  className: string;
+  prefetch?: boolean;
+}
+
+export default function NavLink({
+  href,
+  prefetch = true,
+  children,
+  className,
+}: NavLinkProps) {
+  const pathName = usePathname();
+
+  console.log(pathName);
+
+  return (
+    <Link
+      href={href}
+      prefetch={prefetch}
+      className={className + `${href === pathName ? " underline" : ""}`}
+    >
+      {children}
+    </Link>
+  );
+}
