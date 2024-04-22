dc712b4390440485fbfb38d7109f26cab20a3447
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-04-22 15:52:11 +0330

    practice 1 : handle searchBox auto complete functionality

--- practice1_first-NextJs-app/components/SearchBox.tsx
+++ practice1_first-NextJs-app/components/SearchBox.tsx
@@ -2,25 +2,47 @@
 
 import type { Review } from "@/lib/reviews";
 import { Combobox } from "@headlessui/react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 interface SearchBoxProps {
   reviews: Review[];
 }
 
 export default function SearchBox({ reviews }: SearchBoxProps) {
+  const [query, setQuery] = useState("");
+  const router = useRouter();
+
+  const filteredReviews = reviews.filter(review => review.title.toLowerCase().includes(query.toLocaleLowerCase()))
+
+  const handleChange = (selectedReview: Review) => {
+    router.push(`/reviews/${selectedReview.slug}`);
+  };
+
+
   return (
-    <div className='relative'>
-      <Combobox>
+    <div className="relative">
+      <Combobox onChange={handleChange}>
         <Combobox.Input
           className="p-2 outline-none"
           type="text"
           placeholder="Search ..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
         />
-        <Combobox.Options className='absolute bg-white w-full rounded'>
-          {reviews.map((review) => (
-            <Combobox.Option className='w-full' key={review.slug} value={review.title}>
+        <Combobox.Options className="absolute bg-white w-full rounded max-h-40 overflow-y-scroll">
+          {filteredReviews.map((review) => (
+            <Combobox.Option
+              className="w-full"
+              key={review.slug}
+              value={review}
+            >
               {({ active }) => (
-                <span className={`block px-2 py-1 cursor-pointer truncate ${active ? "bg-orange-100" : ""}`}>
+                <span
+                  className={`block px-2 py-1 cursor-pointer truncate ${
+                    active ? "bg-orange-100" : ""
+                  }`}
+                >
                   {review.title}
                 </span>
               )}
