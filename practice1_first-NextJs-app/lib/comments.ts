29f475adc6313e7c6af6d06ccab3836e701b73ac
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-04-26 20:52:52 +0330

    practice 1 : create comment function and prisma dev server bug

--- practice1_first-NextJs-app/lib/comments.ts
+++ practice1_first-NextJs-app/lib/comments.ts
@@ -1,5 +1,15 @@
 import { db } from "./db";
 
+export async function createComment(user: string, body: string, slug: string) {
+  return await db.comment.create({
+    data: {
+      user,
+      body,
+      slug,
+    },
+  });
+}
+
 export async function getCommentsByReview(slug: string) {
   const comments = await db.comment.findMany({
     where: { slug },
--- practice1_first-NextJs-app/lib/db.ts
+++ practice1_first-NextJs-app/lib/db.ts
@@ -1,3 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 
-export const db = new PrismaClient();
+// Augment the global namespace to define the type of globalThis
+declare global {
+    var prismaClient: PrismaClient | undefined;
+}
+
+function createPrismaClientInstance(): PrismaClient {
+    if (!globalThis.prismaClient) {
+        globalThis.prismaClient = new PrismaClient();
+    }
+    return globalThis.prismaClient;
+}
+
+export const db = createPrismaClientInstance();
