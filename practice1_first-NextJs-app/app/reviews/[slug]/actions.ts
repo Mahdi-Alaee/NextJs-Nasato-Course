1ee36b9723c93554a44b21ddc918273fb3b5e7eb
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-04-29 15:33:27 +0330

    practice 1 : handle validation error

--- practice1_first-NextJs-app/app/reviews/[slug]/actions.ts
+++ practice1_first-NextJs-app/app/reviews/[slug]/actions.ts
@@ -9,15 +9,17 @@ export async function createCommentAction(formData: FormData) {
   const user = formData.get("user") as string;
   const body = formData.get("message") as string;
 
-  if (!user.length) {
-    return { error: true, message: "the user field is invalid" };
-  }
-
   const data = {
     slug,
     user,
     body,
   };
+
+  const error = validate(data);
+
+  if (error !== false) {
+    return { isError: true, message: error };
+  }
 
   const res = await db.comment.create({
     data,
@@ -25,4 +27,28 @@ export async function createCommentAction(formData: FormData) {
   console.log(res);
   revalidatePath(`/reviews/${data.slug}`);
   redirect(`/reviews/${data.slug}`);
+}
+
+interface Data {
+  slug: string;
+  user: string;
+  body: string;
+}
+
+function validate(data: Data) {
+  if (data.user.length < 5) {
+    return "the user field must have at least 5 letter!";
+  } 
+   else if (data.user.length > 50) {
+    return "the user field is too long!";
+  } 
+   else if (data.body.length < 10) {
+    return "the message field must have at least 10 letter!";
+  } 
+   else if (data.body.length < 10) {
+    return "the message field is too long!";
+  } 
+  else {
+    return false;
+  }
 }
--- practice1_first-NextJs-app/components/CommentForm.tsx
+++ practice1_first-NextJs-app/components/CommentForm.tsx
@@ -1,22 +1,32 @@
 "use client";
 
 import { createCommentAction } from "@/app/reviews/[slug]/actions";
-import { FormEvent, FormEventHandler } from "react";
+import { FormEvent, FormEventHandler, useState } from "react";
 
 interface CommentFormProps {
   slug: string;
   title: string;
 }
 
+interface Error {
+  isError: boolean;
+  message: string;
+}
+
 export default function CommentForm({ slug, title }: CommentFormProps) {
+  const [error, setError] = useState<Error | null>(null);
+
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     const form = e.currentTarget;
     const formData = new FormData(form);
     const res = await createCommentAction(formData);
-    if(res?.error){
-      alert(res.message);
+    if (res?.isError) {
+      setError(res);
+    } else {
+      setError(null);
+      form.reset();
     }
   };
 
@@ -39,9 +49,9 @@ export default function CommentForm({ slug, title }: CommentFormProps) {
             className="border border-gray-200 outline-none p-2 md:w-96"
             type="text"
             name="user"
-            minLength={5}
-            maxLength={50}
-            required
+            // minLength={5}
+            // maxLength={50}
+            // required
           />
         </div>
         {/* input wrapper */}
@@ -52,11 +62,14 @@ export default function CommentForm({ slug, title }: CommentFormProps) {
           <textarea
             name="message"
             className="border border-gray-200 outline-none p-2 min-h-28 max-h-96 md:w-96"
-            minLength={10}
-            maxLength={500}
-            required
+            // minLength={10}
+            // maxLength={500}
+            // required
           ></textarea>
         </div>
+        {Boolean(error) && (
+          <p className="text-red-700 text-center pl-6">{error?.message}</p>
+        )}
         <button
           className="bg-orange-800 text-white mx-auto px-12 py-2 rounded-lg"
           type="submit"
