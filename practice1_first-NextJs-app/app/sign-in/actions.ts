07376ee6e6e01010dc125720238186746a828fa7
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-05-01 22:14:20 +0330

    practice 1 : rudimentry athentication

--- practice1_first-NextJs-app/app/sign-in/actions.ts
+++ practice1_first-NextJs-app/app/sign-in/actions.ts
@@ -1,5 +1,19 @@
-'use server';
+"use server";
 
-export async function signInAction(formData: FormData){
-    console.log(formData);
+import { redirect } from "next/navigation";
+
+export async function signInAction(formData: FormData) {
+  const email = formData.get("email") as string;
+  const password = formData.get("password") as string;
+  const user = athenticate(email, password);
+  if (!user) {
+    return { isError: true, message: "incorrect data is entered !!!" };
+  }
+  redirect('/');
+}
+
+function athenticate(email: string, password: string) {
+  if (email.endsWith("@example.com") && password === "test") {
+    return { email, password };
+  }
 }
