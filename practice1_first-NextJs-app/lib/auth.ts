c98bfc8b60a2118bca0748f75bcf895222a255a2
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-05-03 10:26:31 +0330

    practice 1 : set expire date for cookie and token

--- practice1_first-NextJs-app/lib/auth.ts
+++ practice1_first-NextJs-app/lib/auth.ts
@@ -1,31 +1,34 @@
 import { JWTPayload, SignJWT, jwtVerify } from "jose";
 import { cookies } from "next/headers";
 
-
 const JWT_SECRET = new TextEncoder().encode(process.env.JWT_SECRET);
 const COOKIE_NAME = "sessionToken";
+const DURATION_DATE = 14 * 24 * 60 * 60 * 1000;
 
 export type AuthenticatedUser = {
   email: string;
-}
+};
 
 export async function getUserFromSession() {
-    const userToken = cookies().get(COOKIE_NAME);
-    if (!userToken) {
-        return null;
-    }
-    
-    const { payload } = (await jwtVerify(userToken.value, JWT_SECRET)) as {
-        payload: AuthenticatedUser;
-    };
-    
-    return payload;
+  const userToken = cookies().get(COOKIE_NAME);
+  if (!userToken) {
+    return null;
+  }
+
+  const { payload } = (await jwtVerify(userToken.value, JWT_SECRET)) as {
+    payload: AuthenticatedUser;
+  };
+
+  return payload;
 }
 
 export async function setUserSession(user: AuthenticatedUser) {
+  const expireDate = new Date(Date.now() + DURATION_DATE);
+
   const token = await new SignJWT(user)
     .setProtectedHeader({ alg: "HS256" })
+    .setExpirationTime(expireDate)
     .sign(JWT_SECRET);
 
-  cookies().set(COOKIE_NAME, token);
+  cookies().set(COOKIE_NAME, token, { expires: expireDate });
 }
