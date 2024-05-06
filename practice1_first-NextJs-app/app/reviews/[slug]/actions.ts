77fb262dd373becb0ef223220c57ddca311cb939
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-05-06 17:16:37 +0330

    practice 1 : set db relation for comment and user

--- practice1_first-NextJs-app/app/reviews/[slug]/actions.ts
+++ practice1_first-NextJs-app/app/reviews/[slug]/actions.ts
@@ -1,17 +1,24 @@
 "use server";
 
+import { getUserFromSession } from "@/lib/auth";
 import { db } from "@/lib/db";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
 export async function createCommentAction(formData: FormData) {
   const slug = formData.get("slug") as string;
-  const user = formData.get("user") as string;
   const body = formData.get("message") as string;
+  const user = await getUserFromSession();
+
+  if (!user) {
+    throw new Error(
+      "the user is not authenticated!!!\n did you wanna hack my website???😂"
+    );
+  }
 
   const data = {
     slug,
-    user,
+    userId: user.id,
     body,
   };
 
@@ -31,16 +38,17 @@ export async function createCommentAction(formData: FormData) {
 
 interface Data {
   slug: string;
-  user: string;
+  userId: string;
   body: string;
 }
 
 function validate(data: Data) {
-  if (data.user.length < 5) {
-    return "the user field must have at least 5 letter!";
-  } else if (data.user.length > 50) {
-    return "the user field is too long!";
-  } else if (data.body.length < 10) {
+  // if (data.user.length < 5) {
+  //   return "the user field must have at least 5 letter!";
+  // } else if (data.user.length > 50) {
+  //   return "the user field is too long!";
+  // }
+  if (data.body.length < 10) {
     return "the message field must have at least 10 letter!";
   } else if (data.body.length < 10) {
     return "the message field is too long!";
