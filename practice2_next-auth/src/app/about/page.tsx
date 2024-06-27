df9c4207d1b5d09f8b025f800dea0291f42a25de
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-06-27 21:19:19 +0330

    practice 2 : server component url protection

--- practice2_next-auth/src/app/about/page.tsx
+++ practice2_next-auth/src/app/about/page.tsx
@@ -1,18 +1,25 @@
-"use client";
+// 'use client'
 
 import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth/next";
+import { options } from "../api/auth/[...nextauth]/options";
 
-export default function Login() {
+export default async function Login() {
   //! client component way
-  const { data: session } = useSession({
-    required: true,
-    onUnauthenticated() {
-      redirect("/auth/login?callbackUrl=/about");
-    },
-  });
+  // const { data: session } = useSession({
+  //   required: true,
+  //   onUnauthenticated() {
+  //     redirect("/auth/login?callbackUrl=/about");
+  //   },
+  // });
 
-  console.log(session?.user);
+  //! server component way
+  const session = await getServerSession(options);
+  console.log(session);
 
+  if (!session) {
+    redirect("/auth/login?callbackUrl=/about");
+  }
   return <h1 className="text-4xl">About Page! && {session?.user?.email}</h1>;
 }
